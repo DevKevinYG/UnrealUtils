63d857946362e92348458d3721fb1bf8c70a4e95
DevKevinYG <66486658+DevKevinYG@users.noreply.github.com>
2021-09-09 00:12:20 -0500

    Add files via upload

---
+++ NVTraceHelper.cpp
@@ -0,0 +1,168 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Lib/NVTraceHelper.h"
+#include "Engine/World.h"
+#include "PhysicsEngine/PhysicsSettings.h"
+#include "Runtime\Engine\Private\KismetTraceUtils.h"
+
+static const float CUSTOM_KISMET_TRACE_DEBUG_IMPACTPOINT_SIZE = 16.f;
+
+bool UNVTraceHelper::CustomCapsuleTraceSingleForObjects(const UObject* WorldContextObject, const FVector Center, const FRotator Rotation, float Radius, float HalfHeight, const TArray<TEnumAsByte<EObjectTypeQuery>>& ObjectTypes,
+	bool bTraceComplex, const TArray<AActor*>& ActorsToIgnore, EDrawDebugTrace::Type DrawDebugType, FHitResult& OutHit, bool bIgnoreSelf, FLinearColor TraceColor, FLinearColor TraceHitColor, float DrawTime)
+{
+	static const FName CapsuleTraceSingleName(TEXT("CapsuleTraceSingleForObjects"));
+	FCollisionQueryParams Params = CustomConfigureCollisionParams(CapsuleTraceSingleName, bTraceComplex, ActorsToIgnore, bIgnoreSelf, WorldContextObject);
+
+	FCollisionObjectQueryParams ObjectParams = CustomConfigureCollisionObjectParams(ObjectTypes);
+	if (ObjectParams.IsValid() == false)
+	{
+		UE_LOG(LogBlueprintUserMessages, Warning, TEXT("Invalid object types"));
+		return false;
+	}
+
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+	bool const bHit = World ? World->SweepSingleByObjectType(OutHit, Center, Center, Rotation.Quaternion(), ObjectParams, FCollisionShape::MakeCapsule(Radius, HalfHeight), Params) : false;
+
+#if ENABLE_DRAW_DEBUG //DRAW DEBUG CAPSULE
+	if (DrawDebugType != EDrawDebugTrace::None)
+	{
+		bool bPersistent = DrawDebugType == EDrawDebugTrace::Persistent;
+		float LifeTime = (DrawDebugType == EDrawDebugTrace::ForDuration) ? DrawTime : 0.f;
+
+		if (bHit && OutHit.bBlockingHit)
+		{
+			// Red up to the blocking hit, green thereafter
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugCapsule(World, OutHit.Location, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugLine(World, Center, OutHit.Location, TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugPoint(World, OutHit.ImpactPoint, CUSTOM_KISMET_TRACE_DEBUG_IMPACTPOINT_SIZE, TraceColor.ToFColor(true), bPersistent, LifeTime);
+
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceHitColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugLine(World, OutHit.Location, Center, TraceHitColor.ToFColor(true), bPersistent, LifeTime);
+		}
+		else
+		{
+			// no hit means all red
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugLine(World, Center, Center, TraceColor.ToFColor(true), bPersistent, LifeTime);
+		}
+	}
+#endif
+
+	return bHit;
+}
+
+bool UNVTraceHelper::CustomCapsuleTraceMultiForObjects(const UObject* WorldContextObject, const FVector Center, const FRotator Rotation, float Radius, float HalfHeight, const TArray<TEnumAsByte<EObjectTypeQuery>>& ObjectTypes, bool bTraceComplex,
+	const TArray<AActor*>& ActorsToIgnore, EDrawDebugTrace::Type DrawDebugType, TArray<FHitResult>& OutHits, bool bIgnoreSelf, FLinearColor TraceColor, FLinearColor TraceHitColor, float DrawTime)
+{
+	static const FName CapsuleTraceMultiName(TEXT("CapsuleTraceMultiForObjects"));
+	FCollisionQueryParams Params = CustomConfigureCollisionParams(CapsuleTraceMultiName, bTraceComplex, ActorsToIgnore, bIgnoreSelf, WorldContextObject);
+
+	FCollisionObjectQueryParams ObjectParams = CustomConfigureCollisionObjectParams(ObjectTypes);
+	if (ObjectParams.IsValid() == false)
+	{
+		UE_LOG(LogBlueprintUserMessages, Warning, TEXT("Invalid object types"));
+		return false;
+	}
+
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+	bool const bHit = World ? World->SweepMultiByObjectType(OutHits, Center, Center, Rotation.Quaternion(), ObjectParams, FCollisionShape::MakeCapsule(Radius, HalfHeight), Params) : false;
+
+#if ENABLE_DRAW_DEBUG
+	if (DrawDebugType != EDrawDebugTrace::None)
+	{
+		bool bPersistent = DrawDebugType == EDrawDebugTrace::Persistent;
+		float LifeTime = (DrawDebugType == EDrawDebugTrace::ForDuration) ? DrawTime : 0.f;
+
+		if (bHit && OutHits.Last().bBlockingHit)
+		{
+			// Red up to the blocking hit, green thereafter
+			FVector const BlockingHitPoint = OutHits.Last().Location;
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugCapsule(World, BlockingHitPoint, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugLine(World, Center, BlockingHitPoint, TraceColor.ToFColor(true), bPersistent, LifeTime);
+
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceHitColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugLine(World, BlockingHitPoint, Center, TraceHitColor.ToFColor(true), bPersistent, LifeTime);
+		}
+		else
+		{
+			// no hit means all red
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation.Quaternion(), TraceColor.ToFColor(true), bPersistent, LifeTime);
+			::DrawDebugLine(World, Center, Center, TraceColor.ToFColor(true), bPersistent, LifeTime);
+		}
+
+		// draw hits
+		for (int32 HitIdx = 0; HitIdx < OutHits.Num(); ++HitIdx)
+		{
+			FHitResult const& Hit = OutHits[HitIdx];
+			::DrawDebugPoint(World, Hit.ImpactPoint, CUSTOM_KISMET_TRACE_DEBUG_IMPACTPOINT_SIZE, (Hit.bBlockingHit ? TraceColor.ToFColor(true) : TraceHitColor.ToFColor(true)), bPersistent, LifeTime);
+		}
+	}
+#endif
+
+	return bHit;
+}
+
+FCollisionQueryParams UNVTraceHelper::CustomConfigureCollisionParams(FName TraceTag, bool bTraceComplex, const TArray<AActor*>& ActorsToIgnore, bool bIgnoreSelf, const UObject* WorldContextObject)
+{
+	FCollisionQueryParams Params(TraceTag, SCENE_QUERY_STAT_ONLY(KismetTraceUtils), bTraceComplex);
+	Params.bReturnPhysicalMaterial = true;
+	Params.bReturnFaceIndex = !UPhysicsSettings::Get()->bSuppressFaceRemapTable; // Ask for face index, as long as we didn't disable globally
+	Params.AddIgnoredActors(ActorsToIgnore);
+	if (bIgnoreSelf)
+	{
+		const AActor* IgnoreActor = Cast<AActor>(WorldContextObject);
+		if (IgnoreActor)
+		{
+			Params.AddIgnoredActor(IgnoreActor);
+		}
+		else
+		{
+			// find owner
+			const UObject* CurrentObject = WorldContextObject;
+			while (CurrentObject)
+			{
+				CurrentObject = CurrentObject->GetOuter();
+				IgnoreActor = Cast<AActor>(CurrentObject);
+				if (IgnoreActor)
+				{
+					Params.AddIgnoredActor(IgnoreActor);
+					break;
+				}
+			}
+		}
+	}
+
+	return Params;
+}
+
+FCollisionObjectQueryParams UNVTraceHelper::CustomConfigureCollisionObjectParams(const TArray<TEnumAsByte<EObjectTypeQuery>>& ObjectTypes)
+{
+	TArray<TEnumAsByte<ECollisionChannel>> CollisionObjectTraces;
+	CollisionObjectTraces.AddUninitialized(ObjectTypes.Num());
+
+	for (auto Iter = ObjectTypes.CreateConstIterator(); Iter; ++Iter)
+	{
+		CollisionObjectTraces[Iter.GetIndex()] = UEngineTypes::ConvertToCollisionChannel(*Iter);
+	}
+
+	FCollisionObjectQueryParams ObjectParams;
+	for (auto Iter = CollisionObjectTraces.CreateConstIterator(); Iter; ++Iter)
+	{
+		const ECollisionChannel & Channel = (*Iter);
+		if (FCollisionObjectQueryParams::IsValidObjectQuery(Channel))
+		{
+			ObjectParams.AddObjectTypesToQuery(Channel);
+		}
+		else
+		{
+			UE_LOG(LogBlueprintUserMessages, Warning, TEXT("%d isn't valid object type"), (int32)Channel);
+		}
+	}
+
+	return ObjectParams;
+}
---
+++ NVTraceHelper.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "NVTraceHelper.generated.h"
+
+/**
+ * This class contains custom traces like the ability to perform capsule traces with rotation
+ */
+UCLASS()
+class CRAFTINGGAME_API UNVTraceHelper : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+
+public:
+	/**
+	* Custom capsule trace collision that uses Center and Rotation as inputs.
+	* 
+	* @param WorldContext	World context
+	* @param Center			The center where this capsule will spawn.
+	* @param Rotation		Rotation of the capsule
+	* @param Radius		Radius of the capsule to sweep
+	* @param HalfHeight	Distance from center of capsule to tip of hemisphere endcap.
+	* @param ObjectTypes	Array of Object Types to trace 
+	* @param bTraceComplex	True to test against complex collision, false to test against simplified collision.
+	* @param OutHit		Properties of the trace hit.
+	* @return				True if there was a hit, false otherwise.
+	*/
+	UFUNCTION(BlueprintCallable, Category="Helper", meta=(bIgnoreSelf="true", WorldContext="WorldContextObject", AutoCreateRefTerm="ActorsToIgnore", DisplayName = "CapsuleTraceForObjects(Rotation)", AdvancedDisplay="TraceColor,TraceHitColor,DrawTime", Keywords="sweep"))
+	static bool CustomCapsuleTraceSingleForObjects(const UObject* WorldContextObject, const FVector Center, const FRotator Rotation, float Radius, float HalfHeight, const TArray<TEnumAsByte<EObjectTypeQuery> > & ObjectTypes,
+		bool bTraceComplex, const TArray<AActor*>& ActorsToIgnore, EDrawDebugTrace::Type DrawDebugType, FHitResult& OutHit, bool bIgnoreSelf, FLinearColor TraceColor = FLinearColor::Red, FLinearColor TraceHitColor = FLinearColor::Green, float DrawTime = 5.0f);
+	
+	/**
+	* Custom capsule multitrace collision that uses Center and Rotation as inputs.
+	* 
+	* @param WorldContext	World context
+	* @param Center			The center where this capsule will spawn.
+	* @param Rotation		Rotation of the capsule
+	* @param Radius		Radius of the capsule to sweep
+	* @param HalfHeight	Distance from center of capsule to tip of hemisphere endcap.
+	* @param ObjectTypes	Array of Object Types to trace 
+	* @param bTraceComplex	True to test against complex collision, false to test against simplified collision.
+	* @param OutHits		A list of hits, sorted along the trace from start to finish.  The blocking hit will be the last hit, if there was one.
+	* @return				True if there was a hit, false otherwise.
+	*/
+	UFUNCTION(BlueprintCallable, Category="Helper", meta=(bIgnoreSelf="true", WorldContext="WorldContextObject", AutoCreateRefTerm="ActorsToIgnore", DisplayName = "MultiCapsuleTraceForObjects(Rotation)", AdvancedDisplay="TraceColor,TraceHitColor,DrawTime", Keywords="sweep"))
+	static bool CustomCapsuleTraceMultiForObjects(const UObject* WorldContextObject, const FVector Center, const FRotator Rotation, float Radius, float HalfHeight, const TArray<TEnumAsByte<EObjectTypeQuery> > & ObjectTypes, bool bTraceComplex,
+		const TArray<AActor*>& ActorsToIgnore, EDrawDebugTrace::Type DrawDebugType, TArray<FHitResult>& OutHits, bool bIgnoreSelf, FLinearColor TraceColor = FLinearColor::Red, FLinearColor TraceHitColor = FLinearColor::Green, float DrawTime = 5.0f);
+
+private:
+
+	static FCollisionQueryParams CustomConfigureCollisionParams(FName TraceTag, bool bTraceComplex, const TArray<AActor*>& ActorsToIgnore, bool bIgnoreSelf, const UObject* WorldContextObject);
+
+	static FCollisionObjectQueryParams CustomConfigureCollisionObjectParams(const TArray<TEnumAsByte<EObjectTypeQuery> > & ObjectTypes);
+	
+};
